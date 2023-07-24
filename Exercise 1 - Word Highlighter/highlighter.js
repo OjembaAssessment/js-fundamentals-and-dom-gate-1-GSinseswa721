6193eefcdcc00d712d3cecb2f38eaed2a4638ab2
Gsinseswa721 <glorianiyonkurusinseswa@gmail.com>
2023-07-24 13:12:59 +0200

    update

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -13,4 +13,22 @@ function textCount(text) {
     const  textSorted= Object.keys(textCount).sort((a, b) => textCount[b] - textCount[a]);
     return textSorted.slice(0, 5);
   }
+  function highstatement(paragraph, topWords) {
+    const words = paragraph.split(/\s+/);
+  
+    const highlighteStatment = words.map(word => {
+      const cleanedWord = word.replace(/[.,!?]/g, '');
+      if (topWords.includes(cleanedWord)) {
+        if (word.charAt(0) === word.charAt(0).toUpperCase()) {
+          return `<span class="highlight underline">${word}</span>`;
+        } else {
+          return `<span class="highlight">${word}</span>`;
+        }
+      } else {
+        return word;
+      }
+    }).join(' ');
+  
+    return highlighteStatment;
+  }
   
