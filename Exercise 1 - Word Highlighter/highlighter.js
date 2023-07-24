518827ce3b030ead96a8d0a6c849b9738d0ce068
Gsinseswa721 <glorianiyonkurusinseswa@gmail.com>
2023-07-24 12:07:04 +0200

    update

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,11 @@
-console.log("Exercise 1 - Word Highlighter");
+//console.log("Exercise 1 - Word Highlighter");
+
+document.addEventListener("DOMContentLoaded", function(){
+    const paragraphe = document.getElementById("myParagraph");
+    var text = str.split(/\b/);
+    for(var i = 0; i < words.length; i++)
+    wordCounts["_" + words[i]] = (wordCounts["_" + words[i]] || 0) + 1;
+    }
+
+    
+);
--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,39 @@
-console.log("Exercise 3 - Timer");
+const mainTitle = document.querySelector('#title');
+let curValue = 0;
+const timerElement = document.getElementById("title")
+const btnDecrement = document.querySelector('#decrement');
+const btnIncrement = document.querySelector('#increment');
+const btnReset = document.querySelector('#reset');
+
+function updateTimerDisplay() {
+    timerElement.textContent = curValue;
+  }
+
+function startCountdown() {
+    const countdownInterval = setInterval(() => {
+      curValue--;
+      updateTimerDisplay();
+
+      if (curValue <= 0) {
+        clearInterval(countdownInterval);
+        hideButtons();
+        timerElement.textContent = "0";
+      }
+    }, 1000);
+  }
+
+btnIncrement.addEventListener('click', () => {
+    curValue ++;
+    mainTitle.textContent = curValue;
+});
+
+btnDecrement.addEventListener('click', () => {
+    curValue --;
+    mainTitle.textContent = curValue;
+});
+
+btnReset.addEventListener('click', () => {
+    curValue = 0;
+    mainTitle.textContent = curValue;
+});
+
